fcf4f27be3e4a2a10c4fa732dff8c159255dd7ba
Chuli <chuliantunez@gmail.com>
2023-07-02 18:45:35 -0300

    03-functions

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,10 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+  <div class="container">
+    <h1 class="title">Solid Principles and Clean Code</h1>
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+    <h3 class="subtitle">check console</h3>
+  </div>
+`;
